a6bb61b332c6b6d7e4e2d69d5403e6b5eea7ab5b
whichxjy <whichxjy@gmail.com>
2020-11-20 03:22:47 +0800

    Int zk client

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+extern crate zookeeper;
+
 use serde::{Deserialize, Serialize};
+use std::str;
+use std::time::Duration;
+use zookeeper::{Acl, CreateMode, WatchedEvent, Watcher, ZooKeeper};
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all(serialize = "camelCase", deserialize = "camelCase"))]
@@ -13,18 +18,49 @@ struct ProcessConfig {
     version: String,
 }
 
+struct LoggingWatcher;
+impl Watcher for LoggingWatcher {
+    fn handle(&self, e: WatchedEvent) {
+        println!("{:?}", e)
+    }
+}
+
 fn main() {
+    let zk_urls = "localhost:2181";
+
+    let zk_client = ZooKeeper::connect(&*zk_urls, Duration::from_secs(15), LoggingWatcher).unwrap();
+
+    let path = "/config";
+
     let data = r#"
-        {
-            "configs": [
-                {
-                    "name": "hello",
-                    "version": "1"
-                }
-           ]
-        }"#;
+    {
+        "configs": [
+            {
+                "name": "hello",
+                "version": "1"
+            }
+       ]
+    }"#;
 
-    let wdr_config: WdrConfig = serde_json::from_str(&data).unwrap();
+    if zk_client.exists(path, false).unwrap().is_none() {
+        // Create a new node and write config.
+        if let Err(err) = zk_client.create(
+            path,
+            data.as_bytes().to_vec(),
+            Acl::open_unsafe().clone(),
+            CreateMode::Persistent,
+        ) {
+            println!("{:?}", err);
+        }
+    }
 
-    println!("deserialized = {:?}", wdr_config);
+    // Read config.
+    match zk_client.get_data(path, false) {
+        Ok((config_data, _)) => {
+            let config_data = str::from_utf8(&config_data).unwrap();
+            let wdr_config: WdrConfig = serde_json::from_str(config_data).unwrap();
+            println!("deserialized = {:?}", wdr_config);
+        }
+        Err(err) => println!("{:?}", err),
+    }
 }
