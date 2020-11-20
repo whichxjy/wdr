14eb8e9195871e7455c714b7241f4e8f10671abd
whichxjy <whichxjy@gmail.com>
2020-11-21 06:58:08 +0800

    Add expect message for downloaded path

--- src/config.rs
+++ src/config.rs
@@ -4,7 +4,7 @@ use std::path::Path;
 lazy_static! {
     pub static ref DOWNLOADED_PATH: &'static Path = {
         let downloaded_path = Path::new("downloaded");
-        let _ = fs::create_dir_all(downloaded_path);
+        fs::create_dir_all(downloaded_path).expect("Fail to create downloaded directory");
         downloaded_path
     };
 }
