505185d09bffbefa6be024dfbbfe56a64bb27954
whichxjy <whichxjy@gmail.com>
2020-11-21 18:11:06 +0800

    Update logger

--- src/logger.rs
+++ src/logger.rs
@@ -1,32 +1,29 @@
-#[macro_export]
+#![allow(unused)]
+
 macro_rules! wdr_trace {
     ($x:expr $(, $($y:expr),+)?) => {
         trace!(concat!("[{}] ", $x), function_name!() $(, $($y),+)?);
     };
 }
 
-#[macro_export]
 macro_rules! wdr_debug {
     ($x:expr $(, $($y:expr),+)?) => {
         debug!(concat!("[{}] ", $x), function_name!() $(, $($y),+)?);
     };
 }
 
-#[macro_export]
 macro_rules! wdr_info {
     ($x:expr $(, $($y:expr),+)?) => {
         info!(concat!("[{}] ", $x), function_name!() $(, $($y),+)?);
     };
 }
 
-#[macro_export]
 macro_rules! wdr_warn {
     ($x:expr $(, $($y:expr),+)?) => {
         warn!(concat!("[{}] ", $x), function_name!() $(, $($y),+)?);
     };
 }
 
-#[macro_export]
 macro_rules! wdr_error {
     ($x:expr $(, $($y:expr),+)?) => {
         error!(concat!("[{}] ", $x), function_name!() $(, $($y),+)?);
