39fcc27391f2a027e775162be4f2e4d930494ff1
whichxjy <whichxjy@gmail.com>
2020-11-22 12:19:10 +0800

    Refactor manager

--- src/manager.rs
+++ src/manager.rs
@@ -2,18 +2,20 @@ use crossbeam::channel::tick;
 use std::collections::{HashMap, HashSet};
 use std::str;
 use std::sync::RwLock;
+use std::thread;
 use std::time::Duration;
 use zookeeper::CreateMode;
 
-use crate::config::ZK_CONFIG_PATH;
+use crate::config::{ZK_CONFIG_PATH, ZK_CONNECT_STRING};
 use crate::model::{ProcessConfig, WdrConfig};
 use crate::process::Process;
 use crate::zk::ZkClient;
 
-pub struct Manager {
-    zk_client: ZkClient,
-    prev_wdr_config: WdrConfig,
-    workers_lock: RwLock<HashMap<String, Worker>>,
+lazy_static! {
+    static ref ZK_CLIENT: ZkClient =
+        ZkClient::new(&ZK_CONNECT_STRING).expect("Fail to connect to zk");
+    static ref CURR_WDR_CONFIG_LOCK: RwLock<WdrConfig> = RwLock::new(WdrConfig::default());
+    static ref WORKERS_LOCK: RwLock<HashMap<String, Worker>> = RwLock::new(HashMap::new());
 }
 
 pub struct Worker {
@@ -30,138 +32,129 @@ impl Worker {
     }
 }
 
-impl Manager {
-    pub fn new(zk_client: ZkClient) -> Self {
-        Manager {
-            zk_client,
-            prev_wdr_config: WdrConfig::default(),
-            workers_lock: RwLock::new(HashMap::new()),
-        }
-    }
+pub fn run() {
+    // Check config every 5 seconds.
+    let ticker = tick(Duration::new(5, 0));
 
-    pub fn run(&mut self) {
-        // Check config every 5 seconds.
-        let ticker = tick(Duration::new(5, 0));
+    loop {
+        ticker.recv().unwrap();
 
-        loop {
-            ticker.recv().unwrap();
-
-            let wdr_config = match self.read_config() {
-                Some(wdr_config) => wdr_config,
-                None => {
-                    wdr_error!("Fail to read config:");
-                    continue;
-                }
-            };
-            wdr_debug!("Read config: {:?}", wdr_config);
-
-            if wdr_config != self.prev_wdr_config {
-                self.flush_all_processes(&wdr_config.configs);
-                self.prev_wdr_config = wdr_config;
+        let wdr_config = match read_config() {
+            Some(wdr_config) => wdr_config,
+            None => {
+                wdr_error!("Fail to read config:");
+                continue;
             }
-        }
-    }
+        };
+        wdr_debug!("Read config: {:?}", wdr_config);
 
-    fn read_config(&self) -> Option<WdrConfig> {
-        if !self.zk_client.exists(&ZK_CONFIG_PATH) {
-            // Create a new node.
-            if let Err(err) = self
-                .zk_client
-                .create(&ZK_CONFIG_PATH, CreateMode::Persistent)
+        if wdr_config != *CURR_WDR_CONFIG_LOCK.read().unwrap() {
             {
-                wdr_error!("{}", err);
-                return None;
+                let mut curr_wdr_config = CURR_WDR_CONFIG_LOCK.write().unwrap();
+                *curr_wdr_config = wdr_config;
             }
+
+            let curr_wdr_config = CURR_WDR_CONFIG_LOCK.read().unwrap();
+            flush_all_processes(curr_wdr_config.configs.clone());
         }
+    }
+}
 
-        // Read config.
-        let config_data = match self.zk_client.get_data(&ZK_CONFIG_PATH) {
-            Ok(config_data) => config_data,
-            _ => return None,
-        };
-
-        let config_data = match str::from_utf8(&config_data) {
-            Ok(config_data) => config_data,
-            Err(err) => {
-                wdr_error!("{}", err);
-                return None;
-            }
-        };
-
-        match WdrConfig::from_str(config_data) {
-            Some(wdr_config) => Some(wdr_config),
-            None => None,
+fn read_config() -> Option<WdrConfig> {
+    if !ZK_CLIENT.exists(&ZK_CONFIG_PATH) {
+        // Create a new node.
+        if let Err(err) = ZK_CLIENT.create(&ZK_CONFIG_PATH, CreateMode::Persistent) {
+            wdr_error!("{}", err);
+            return None;
         }
     }
 
-    fn flush_all_processes(&mut self, process_configs: &[ProcessConfig]) {
-        let mut valid_process_names: HashSet<&str> = HashSet::new();
+    // Read config.
+    let config_data = match ZK_CLIENT.get_data(&ZK_CONFIG_PATH) {
+        Ok(config_data) => config_data,
+        _ => return None,
+    };
 
-        for process_config in process_configs {
-            valid_process_names.insert(&process_config.name);
-            self.flush_process(process_config);
+    let config_data = match str::from_utf8(&config_data) {
+        Ok(config_data) => config_data,
+        Err(err) => {
+            wdr_error!("{}", err);
+            return None;
         }
+    };
 
-        self.clear_useless_processes(valid_process_names);
+    match WdrConfig::from_str(config_data) {
+        Some(wdr_config) => Some(wdr_config),
+        None => None,
+    }
+}
+
+fn flush_all_processes(process_configs: Vec<ProcessConfig>) {
+    let mut valid_process_names: HashSet<String> = HashSet::new();
+
+    for process_config in process_configs {
+        valid_process_names.insert(process_config.name.to_owned());
+
+        thread::spawn(move || {
+            flush_process(&process_config);
+        });
     }
 
-    fn flush_process(&mut self, process_config: &ProcessConfig) {
-        if let Some(old_worker) = self
-            .workers_lock
+    clear_useless_processes(valid_process_names);
+}
+
+fn flush_process(process_config: &ProcessConfig) {
+    if let Some(old_worker) = WORKERS_LOCK
+        .write()
+        .unwrap()
+        .get_mut(process_config.name.as_str())
+    {
+        if process_config.version == old_worker.version {
+            return;
+        }
+
+        // Stop old process.
+        old_worker.process.stop();
+    }
+
+    let mut new_process = Process::new(process_config.to_owned());
+
+    // TODO: Retry.
+    if let Err(err) = new_process.prepare() {
+        wdr_error!("{}", err);
+        return;
+    }
+
+    if let Err(err) = new_process.run() {
+        wdr_error!("{}", err);
+        return;
+    }
+
+    WORKERS_LOCK.write().unwrap().insert(
+        process_config.name.to_owned(),
+        Worker::new(new_process, &process_config.version),
+    );
+}
+
+fn clear_useless_processes(valid_process_names: HashSet<String>) {
+    let mut useless_process_names: HashSet<String> = HashSet::new();
+
+    for name in WORKERS_LOCK.read().unwrap().keys() {
+        if !valid_process_names.contains(name.as_str()) {
+            useless_process_names.insert(name.to_owned());
+        }
+    }
+
+    for useless_process_name in useless_process_names {
+        WORKERS_LOCK
             .write()
             .unwrap()
-            .get_mut(process_config.name.as_str())
-        {
-            if process_config.version == old_worker.version {
-                return;
-            }
+            .get_mut(&useless_process_name)
+            .unwrap()
+            .process
+            .stop();
 
-            // Stop old process.
-            old_worker.process.stop();
-        }
-
-        let mut new_process = Process::new(process_config.to_owned());
-
-        // TODO: Retry.
-        if let Err(err) = new_process.prepare() {
-            wdr_error!("{}", err);
-            return;
-        }
-
-        if let Err(err) = new_process.run() {
-            wdr_error!("{}", err);
-            return;
-        }
-
-        self.workers_lock.write().unwrap().insert(
-            process_config.name.to_owned(),
-            Worker::new(new_process, &process_config.version),
-        );
-    }
-
-    fn clear_useless_processes(&mut self, valid_process_names: HashSet<&str>) {
-        let mut useless_process_names: HashSet<String> = HashSet::new();
-
-        for name in self.workers_lock.read().unwrap().keys() {
-            if !valid_process_names.contains(name.as_str()) {
-                useless_process_names.insert(name.to_owned());
-            }
-        }
-
-        for useless_process_name in useless_process_names {
-            self.workers_lock
-                .write()
-                .unwrap()
-                .get_mut(&useless_process_name)
-                .unwrap()
-                .process
-                .stop();
-
-            self.workers_lock
-                .write()
-                .unwrap()
-                .remove(&useless_process_name);
-            wdr_info!("Process {} is clear", useless_process_name);
-        }
+        WORKERS_LOCK.write().unwrap().remove(&useless_process_name);
+        wdr_info!("Process {} is clear", useless_process_name);
     }
 }
