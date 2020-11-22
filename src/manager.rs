83b81428aedc5f47c7f57f13f87c22f199fc9d2b
whichxjy <whichxjy@gmail.com>
2020-11-22 09:22:25 +0800

    Clear useless processes

--- src/manager.rs
+++ src/manager.rs
@@ -1,5 +1,5 @@
 use crossbeam::channel::tick;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::str;
 use std::time::Duration;
 use zookeeper::CreateMode;
@@ -25,8 +25,8 @@ impl Manager {
     }
 
     pub fn run(&mut self) {
-        // Check config every 10 seconds.
-        let ticker = tick(Duration::new(10, 0));
+        // Check config every 5 seconds.
+        let ticker = tick(Duration::new(5, 0));
 
         loop {
             ticker.recv().unwrap();
@@ -80,9 +80,14 @@ impl Manager {
     }
 
     fn flush_all_processes(&mut self, process_configs: &[ProcessConfig]) {
+        let mut valid_process_names: HashSet<&str> = HashSet::new();
+
         for process_config in process_configs {
+            valid_process_names.insert(&process_config.name);
             self.flush_process(process_config);
         }
+
+        self.clear_useless_processes(valid_process_names);
     }
 
     fn flush_process(&mut self, process_config: &ProcessConfig) {
@@ -109,5 +114,23 @@ impl Manager {
 
         self.processes
             .insert(process_config.name.to_owned(), new_process);
+    }
+
+    fn clear_useless_processes(&mut self, valid_process_names: HashSet<&str>) {
+        let mut useless_process_names: HashSet<String> = HashSet::new();
+
+        for name in self.processes.keys() {
+            if !valid_process_names.contains(name.as_str()) {
+                useless_process_names.insert(name.to_owned());
+            }
+        }
+
+        for useless_process_name in useless_process_names {
+            let process_to_clear = self.processes.get_mut(&useless_process_name).unwrap();
+            process_to_clear.stop();
+
+            self.processes.remove(&useless_process_name);
+            wdr_info!("Process {} is clear", useless_process_name);
+        }
     }
 }
